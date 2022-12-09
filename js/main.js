9cb1b209bd5a0902057130a8006abe2cf16da3ec
CoreyOliver <coreyoliver.dev@gmail.com>
2022-12-09 16:38:09 -0500

    canvas set up complete

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/** @type {HTMLCanvasElement} */
+window.addEventListener('load',function() {
+    const canvas = document.getElementById('canvas1')
+    const ctx = canvas.getContext('2d')
+    canvas.height = 720;
+    canvas.width = 800;
+
+
+})
